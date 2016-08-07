69fe7281aad564733cbe75bed8a9569812eaf12d
Aliki Giakou <al.giakou@gmail.com>
2016-08-07 13:14:43 +0100

    Rspec for horizontal and vertical colouring

--- spec/matrix_spec.rb
+++ spec/matrix_spec.rb
@@ -39,4 +39,20 @@ describe 'Matrix' do
     expect(@image.matrix).to eq initial_image
   end
 
+  # V X Y1 Y2 C - Draw a vertical segment of colour C in column X between rows Y1 and Y2 (inclusive).
+  it "draws a vertical segment of colour 'C' in column X between rows Y1 and Y2 (inclusive)" do
+    expected_image = [['O', 'O', 'O', 'O', 'F'], ['O', 'O', 'O', 'O', 'F'], ['O', 'W', 'O', 'O', 'F'], ['O', 'W', 'O', 'O', 'F'], ['O', 'W', 'O', 'O', 'F'], ['O', 'W', 'O', 'O', 'F']]
+    @image.add_colour_vertical(2,3,6,"W")
+    @image.add_colour_vertical(5,1,6,"F")
+    expect(@image.matrix).to eq expected_image
+  end
+
+  # H X1 X2 Y C - Draw a horizontal segment of colour C in row Y between columns X1 and X2 (inclusive).
+  it "draws a horizontal segment of colour 'A' in row Y between columns X1 and X2 (inclusive)" do
+    expected_image = [['O', 'O', 'O', 'O', 'O'], ['O', 'O', 'O', 'O', 'O'], ['A', 'A', 'A', 'A', 'A'], ['O', 'O', 'O', 'O', 'O'], ['O', 'O', 'O', 'O', 'O'], ['O', 'O', 'O', 'B', 'B']]
+    @image.add_colour_horizontal(1,5,3,'A')
+    @image.add_colour_horizontal(4,5,6,'B')
+    expect(@image.matrix).to eq expected_image
+  end
+
 end
