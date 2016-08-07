11da1d14a0262af7baa9d2892580c3b0ac8477e6
Aliki Giakou <al.giakou@gmail.com>
2016-08-07 12:45:09 +0100

    Rspec for colour pixel

--- spec/matrix_spec.rb
+++ spec/matrix_spec.rb
@@ -23,4 +23,20 @@ describe 'Matrix' do
     end
   end
 
+  # L X Y C - Colours the pixel (X,Y) with colour C
+  it "Colours the pixel (X,Y) with colour C" do
+    expected_image = [['F', 'G', 'O', 'O', 'O'], ['O', 'O', 'O', 'O', 'O'], ['O', 'A', 'O', 'O', 'O'], ['O', 'O', 'O', 'O', 'O'], ['O', 'O', 'O', 'O', 'O'], ['O', 'O', 'O', 'O', 'W']]
+    @image.colour_pixel(5,6,'W')
+    @image.colour_pixel(1,1,'F')
+    @image.colour_pixel(2,1,'G')
+    @image.colour_pixel(2,3,'A')
+    expect(@image.matrix).to eq expected_image
+  end
+
+  it "stays unchanged if pixel is out of range" do
+    initial_image = [['O', 'O', 'O', 'O', 'O'], ['O', 'O', 'O', 'O', 'O'], ['O', 'O', 'O', 'O', 'O'], ['O', 'O', 'O', 'O', 'O'], ['O', 'O', 'O', 'O', 'O'], ['O', 'O', 'O', 'O', 'O']]
+    @image.colour_pixel(6,6,'W')
+    expect(@image.matrix).to eq initial_image
+  end
+
 end
